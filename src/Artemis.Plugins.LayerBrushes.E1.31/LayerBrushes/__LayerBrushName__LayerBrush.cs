8dd3d2b5acb38783b63c56cd9d4f5060e79c5024
DJMalachite <nathansacca@gmail.com>
2022-10-01 19:00:55 +0100

    Add Plugin.E1.31Receiver

---
+++ src/Artemis.Plugins.LayerBrushes.E1.31/LayerBrushes/__LayerBrushName__LayerBrush.cs
@@ -0,0 +1,29 @@
+using Artemis.Core.LayerBrushes;
+using Artemis.Plugins.LayerBrushes.E1._31.PropertyGroups;
+using SkiaSharp;
+using System.IO;
+
+namespace Artemis.Plugins.LayerBrushes.E1._31.LayerBrushes
+{
+    public class E1.31LayerBrush : LayerBrush<E1.31PropertyGroup>
+{
+    public override void EnableLayerBrush()
+    {
+
+    }
+
+    public override void DisableLayerBrush()
+    {
+
+    }
+
+    public override void Update(double deltaTime)
+    {
+
+    }
+
+    public override void Render(SKCanvas canvas, SKRect bounds, SKPaint paint)
+    {
+
+    }
+}}
